5f859998f98ec9971de6d9add0ebae75a0ff7d07
Christopher DeLeon <lionscantcode@gmail.com>
2022-07-22 13:22:43 -0500

    Board State Bug Patches (#3)
    Board, playerTurn, and previousPlayersTurn state's are now updating when reset button is clicked - regardless of previous states

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Square.css'
 
 const Square = ({ num = 0, className = 'square', children, onClick }: any) => {
+  // TODO: We will need state keeping track of individual square's clicked state
   return (
     <div className={className} onClick={onClick}>
       <span className="XO_playermove">{children}</span>
