5a3c9d425afe081ce66e76d88411335936ecd7ec
Vroomfrondal <lionscantcode@gmail.com>
2022-07-22 09:02:35 -0500

    Game History Hooks
    - Created state hooks for keeping track of game history, current and previous player
    
    - Reset button resets game state

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,15 +1,10 @@
 import React, { useState } from 'react'
 import './Square.css'
 
-const Square = ({ num = 0, className = '' }) => {
+const Square = ({ num = 0, className = 'square', children, onClick }: any) => {
   return (
-    <div
-      className={className}
-      onClick={() => {
-        alert(num)
-      }}
-    >
-      <span className="XO_playermove">X</span>
+    <div className={className} onClick={onClick}>
+      <span className="XO_playermove">{children}</span>
     </div>
   )
 }
